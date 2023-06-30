0dc4763620bc6dc16b247872b2ab1defa326c321
EdmilsonVasconcelos <edmilson.vasconcelos89@gmail.com>
2023-06-30 10:57:52 -0300

    feat: testing footer

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -2,18 +2,20 @@ import "./index.scss";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      <div className="logo">
+    <footer className="footer" data-testid="footer-container">
+      <div className="logo" data-testid="footer-logo">
         <img
           src={process.env.PUBLIC_URL + "/images/logo.svg"}
           alt="Imagem da logo"
         />
       </div>
 
-      <span className="text">
+      <span className="text" data-testid="footer-text-sorteio">
         Sorteio promovido com a tecnologia da Baby sorteios.
       </span>
-      <span className="text">Todos os direitos reservados.</span>
+      <span className="text" data-testid="footer-text-direitos">
+        Todos os direitos reservados.
+      </span>
     </footer>
   );
 };
