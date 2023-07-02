30a40ab135ba0012646016d25bf924f879bed77d
EdmilsonVasconcelos <edmilson.vasconcelos89@gmail.com>
2023-07-02 13:57:18 -0300

    fix: changing the code

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 import "./index.scss";
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer" data-testid="footer-container">
       <div className="logo" data-testid="footer-logo">
-        <a href="/">
+        <Link to="/">
           <img
             src={process.env.PUBLIC_URL + "/images/logo.svg"}
             alt="Imagem da logo"
           />
-        </a>
+        </Link>
       </div>
 
       <span className="text" data-testid="footer-text-sorteio">
--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -3,56 +3,54 @@ import "./index.scss";
 
 const Header: React.FC = () => {
   return (
-    <>
-      <div className="content">
-        <div className="content-left">
-          <Link to="/">
-            <img
-              src={process.env.PUBLIC_URL + "/images/logo.svg"}
-              alt="Logo baby sorteios"
-            />
-          </Link>
-        </div>
-
-        <div className="content-center">
-          <a href="#" className="links">
-            Como participar
-          </a>
-          <a href="#" className="links">
-            Termos de uso
-          </a>
-        </div>
-        <div className="content-right">
-          <a href="#">
-            <svg
-              width="30"
-              height="30"
-              viewBox="0 0 30 30"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M24 24L6 24"
-                stroke="white"
-                strokeWidth="4"
-                strokeLinecap="round"
-              />
-              <path
-                d="M24 15L6 15"
-                stroke="white"
-                strokeWidth="4"
-                strokeLinecap="round"
-              />
-              <path
-                d="M24 5L6 5"
-                stroke="white"
-                strokeWidth="4"
-                strokeLinecap="round"
-              />
-            </svg>
-          </a>
-        </div>
+    <div className="content">
+      <div className="content-left">
+        <Link to="/">
+          <img
+            src={process.env.PUBLIC_URL + "/images/logo.svg"}
+            alt="Logo baby sorteios"
+          />
+        </Link>
       </div>
-    </>
+
+      <div className="content-center">
+        <a href="#" className="links">
+          Como participar
+        </a>
+        <a href="#" className="links">
+          Termos de uso
+        </a>
+      </div>
+      <div className="content-right">
+        <a href="#">
+          <svg
+            width="30"
+            height="30"
+            viewBox="0 0 30 30"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M24 24L6 24"
+              stroke="white"
+              strokeWidth="4"
+              strokeLinecap="round"
+            />
+            <path
+              d="M24 15L6 15"
+              stroke="white"
+              strokeWidth="4"
+              strokeLinecap="round"
+            />
+            <path
+              d="M24 5L6 5"
+              stroke="white"
+              strokeWidth="4"
+              strokeLinecap="round"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
   );
 };
 
